cbd33cfb3ecb46abe36939ccd118a71471f8ece4
Thanayut Mungumklang <45162648+thanayut-m@users.noreply.github.com>
2025-07-01 15:22:23 +0700

    Add user authentication middleware
    Introduced a new middleware to handle user authentication for protected routes. This ensures that only authenticated users can access certain endpoints.

---
+++ client/src/components/MUI/Inputs/Inputs.tsx
@@ -0,0 +1,44 @@
+import TextField from '@mui/material/TextField';
+import type { FieldValues, Path, UseFormRegister, FieldErrors } from 'react-hook-form';
+import type { TextFieldProps } from '@mui/material/TextField';
+
+interface InputsProps<T extends FieldValues> {
+  register: UseFormRegister<T>;
+  name: Path<T>;
+  label: string;
+  type: string;
+  id?: string;
+  defaultValue?: string;
+  disabled?: boolean;
+  errors?: FieldErrors<T>;
+  size?: TextFieldProps['size'];
+}
+
+export const Inputs = <T extends FieldValues>({
+  register,
+  name,
+  id,
+  label,
+  type,
+  defaultValue = '',
+  disabled = false,
+  errors = {},
+  size = "small"
+}: InputsProps<T>) => {
+  return (
+    <TextField
+      fullWidth
+      {...register(name)}
+      id={id || name}
+      label={label}
+      type={type}
+      defaultValue={defaultValue}
+      variant="outlined"
+      disabled={disabled}
+      autoComplete="new-password"
+      error={!!errors[name]}
+      helperText={errors[name]?.message?.toString() || ""}
+      size={size}
+    />
+  )
+}
