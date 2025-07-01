e556e1d12946e1dde726b8a3326cc82816667bcc
Thanayut Mungumklang <45162648+thanayut-m@users.noreply.github.com>
2025-07-01 17:55:17 +0700

    Add and integrate FromRadiosGroup component

--- client/src/pages/BloodBankGiveList/Mobile/MobileBloodBankReaction.tsx
+++ client/src/pages/BloodBankGiveList/Mobile/MobileBloodBankReaction.tsx
@@ -1,8 +1,6 @@
-import { FromRadios } from "../../../components/MUI/Checkbox/FromRadios"
+import { FromRadiosGroup } from "../../../components/MUI/Checkbox/FromRadiosGroup "
 import { MobilePrivateLayout2 } from "../../../layouts/MobilePrivateLayout2"
 import { CardTitle } from "./MobileBloodBankReaction/CardTitle"
-
-
 
 export const MobileBloodBankReaction = () => {
     // const { register } = useForm({});
@@ -13,10 +11,9 @@ export const MobileBloodBankReaction = () => {
                 <label className="text-[#B5B5B5] text-base"> บันทึกปฏิกิริยาหลังรับเลือด</label>
                 <div className="bg-white">
                     <div className="grid grid-cols-3 gap-3">
-                        <FromRadios />
-                        <FromRadios />
-                        <FromRadios />
+                        <FromRadiosGroup />
                     </div>
+
                 </div>
             </div>
         </MobilePrivateLayout2>
