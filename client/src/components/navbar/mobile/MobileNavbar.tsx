98c54acb66682e57dcc36e96bb23a3670a3f5481
Thanayut Mungumklang <45162648+thanayut-m@users.noreply.github.com>
2025-07-18 15:44:51 +0700

    Refactor mobile navbar and remove MobileNavbar2

--- client/src/components/navbar/mobile/MobileNavbar.tsx
+++ client/src/components/navbar/mobile/MobileNavbar.tsx
@@ -1,27 +1,25 @@
-import { useLocation } from "react-router";
+import { useLocation } from "react-router"
 
 export const MobileNavbar = () => {
     const location = useLocation();
 
     const getTitle = () => {
         switch (location.pathname) {
+            case "/bloodReaction":
+                return "บันทึกข้อมูลปฏิกิริยาหลังรับเลือด";
+            case "/bloodGiveDetail":
+                return "บันทึกการให้โลหิต";
             case "/giveBloodList":
                 return "ให้เลือดผู้ป่วย";
-            case "/search":
-                return "ค้นหา";
-            case "/MobileBloodBankGiveDetail":
-                return "ธนาคารเลือด";
-            case "/BloodBankReaction":
-                return "บันทึกข้อมูลปฏิกิริยาหลังรับเลือด";
             default:
                 return "Blood Bank";
         }
-    };
+    }
+
     return (
-        <div className="bg-[#0D47A1] shadow-xl text-white">
-            <div className="flex justify-center items-center py-3">
-                <span className="text-xl font-semibold">{getTitle()}</span>
-            </div>
+        <div
+            className="pt-7 pb-2 text-center text-xl text-white font-prompt ">
+            {getTitle()}
         </div>
 
     )
--- client/src/layouts/MobilePrivateLayout.tsx
+++ client/src/layouts/MobilePrivateLayout.tsx
@@ -1,9 +1,8 @@
-import { MobileNavbar2 } from "../components/navbar/mobile/MobileNavbar2"
+import { MobileNavbar } from "../components/navbar/mobile/MobileNavbar"
 
 interface MobilePrivateLayout2Props {
     children: React.ReactNode
 }
-
 
 export const MobilePrivateLayout = ({
     children
@@ -13,7 +12,7 @@ export const MobilePrivateLayout = ({
             <div className="fixed top-0 left-0 w-full h-[120px] bg-[#FF7726] -z-10" />
 
             <div className="fixed bg-[#FF7726] top-0 left-0 w-full z-50">
-                <MobileNavbar2 />
+                <MobileNavbar />
             </div>
 
             <main className="pt-[80px] relative z-0 pb-8">
